39824b29838c13837f97be9aeb4c0e1c404978af
Roger Boardman <roger@bxthree.com>
2013-03-06 10:10:06 +0000

    Added a simple base template and a view to serve it

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url
-
+from django.views.generic import TemplateView
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
@@ -9,6 +9,7 @@ admin.autodiscover()
 # See: https://docs.djangoproject.com/en/dev/topics/http/urls/
 urlpatterns = patterns('',
     # Admin panel and documentation:
+    url(r'^$', TemplateView.as_view(template_name='base.html')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
